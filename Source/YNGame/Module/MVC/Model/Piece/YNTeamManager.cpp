6ee126ccb199077032baa6172aed78b5cb9338b6
규 서문 <illsi@naver.com>
2025-03-21 16:12:49 +0900

    팀 매니저 추가

---
+++ Source/YNGame/Module/MVC/Model/Piece/YNTeamManager.cpp
@@ -0,0 +1,35 @@
+﻿#include "YNTeamManager.h"
+
+FYNTeamManager::FYNTeamManager()
+{
+}
+
+FYNTeamManager::~FYNTeamManager()
+{
+}
+
+void FYNTeamManager::Move(const FYNPathResult& path)
+{
+}
+
+void FYNTeamManager::AddPiece(const int32 teamIndex)
+{
+}
+
+int32 FYNTeamManager::FindStartNodeId() const
+{
+	// TODO
+	return 0;
+}
+
+FYNPieceContext& FYNTeamManager::GetPieceContext(const int32 pieceIndex) const
+{
+	static FYNPieceContext nullPieceContext;
+	return nullPieceContext;
+}
+
+TSharedPtr<FYNPieceModel> FYNTeamManager::findCurrentPiece(const int32 teamIndex) const
+{
+	// TODO
+	return TSharedPtr<FYNPieceModel>();
+}
---
+++ Source/YNGame/Module/MVC/Model/Piece/YNTeamManager.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+#include "YNNodeModel.h"
+
+class FYNPieceModel;
+struct FYNPathResult;
+struct FYNPieceContext;
+
+class FYNTeamManager
+{
+public:
+	FYNTeamManager();
+	~FYNTeamManager();
+
+public:
+	void Move(const FYNPathResult &path);
+	void AddPiece(const int32 teamIndex);
+	int32 FindStartNodeId() const;
+	FYNPieceContext& GetPieceContext(const int32 pieceIndex) const;
+
+private:
+	TSharedPtr<FYNPieceModel> findCurrentPiece(const int32 teamIndex) const;
+};
